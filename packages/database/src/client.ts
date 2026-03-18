06694b699cbfb10e6615c0abc1aafdfc52b90add
kazuki kawahara <kazu.0310123@gmail.com>
2026-03-18 20:03:43 +0900

    refactor: comment out Prisma client initialization to prevent multiple instances in non-production environments

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,7 +1,8 @@
+import { prisma } from ".";
 import { PrismaClient } from "../generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+// export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
